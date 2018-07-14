b0b33578e83d0193c99f80af4fb8624d6477a55c
Z <zleereed@gmail.com>
2018-07-13 23:45:43 -0400

    yo

--- public/js/app.js
+++ public/js/app.js
@@ -1677,7 +1677,7 @@ var GameClient = function () {
             console.log('Connection closed.');
         });
 
-        this.connect('ws://localhost:8001');
+        this.connect('https://aqueous-harbor-83898.herokuapp.com/');
         // optional: send an object to the server with data from client
         //this.connect('ws://localhost:8001', { sessionKey: 'example123456789' })
     }
